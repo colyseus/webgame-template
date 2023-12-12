f32ad941f650977cd0ca1dbf40b81e8fe7f7b973
Endel Dreyer <endel.dreyer@gmail.com>
2023-12-12 17:57:07 -0300

    detect when to use 'localhost' endpoint

--- packages/frontend/src/core/Networking.ts
+++ packages/frontend/src/core/Networking.ts
@@ -1,3 +1,8 @@
 import { Client } from "colyseus.js";
 
-export const client = new Client("http://localhost:2567");
+// detect if we're running on localhost
+const endpoint = (window.location.href.indexOf("localhost") >= 0 || window.location.href.indexOf("127.0.0.1") >= 0)
+  ? "http://localhost:2567"
+  : "https://us-lax-af65ebed.colyseus.dev";
+
+export const client = new Client(endpoint);
