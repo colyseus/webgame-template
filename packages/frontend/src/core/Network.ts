f70d548b8974193f6fd8db2111875b7b124b21fd
Endel Dreyer <endel.dreyer@gmail.com>
2024-03-20 14:26:50 -0300

    update @colyseus/tools for Discord Activity fix inside Colyseus Cloud

--- packages/frontend/src/core/Network.ts
+++ packages/frontend/src/core/Network.ts
@@ -1,12 +1,12 @@
 import { Client, Room } from "colyseus.js";
 import type { MyRoomState } from "../../../backend/src/rooms/MyRoom";
 
-// // detect if we're running on localhost
-// const endpoint = (window.location.href.indexOf("localhost") >= 0 || window.location.href.indexOf("127.0.0.1") >= 0)
-//   ? "http://localhost:2567"
-//   : "https://us-lax-af65ebed.colyseus.dev";
+// detect if we're running on localhost
+const endpoint = (window.location.href.indexOf("localhost") >= 0 || window.location.href.indexOf("127.0.0.1") >= 0)
+  ? "http://localhost:2567"
+  : "https://" + window.location.hostname;
 
 export default {
-  client: new Client(),
+  client: new Client(endpoint),
   room: null as Room<MyRoomState> | null,
 }
